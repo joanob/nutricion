a0c715d8f171c6c8751a02e010b214ffecd3a013
joanob <54626555+joanob@users.noreply.github.com>
2022-08-21 18:11:46 +0200

    dataset changes

--- src/services/dataset.ts
+++ src/services/dataset.ts
@@ -1,10 +1,9 @@
-import {PROTEINA, GLUC_COMP, GLUC_SIMP, LIPIDOS, VITAMINAS, SALES, AGUA_BAJO, AGUA_MEDIO, AGUA_ALTO, Agua, Minerals, Vitamins, Macro, Micro} from "./nutrients"
+import {PROTEINA, GLUC_COMP, GLUC_SIMP, LIPIDOS, K, AGUA_BAJO, AGUA_MEDIO, AGUA_ALTO, Agua, Minerals, Vitamins, Macro, CA} from "./nutrients"
 
 export interface Food {
     name: string;
     list: {
         macro?: Macro[],
-        micro?: Micro[],
         agua?: Agua,
         fibra?: boolean,
         minerals?: Minerals[],
@@ -18,7 +17,8 @@ const dataset: Food[] =  [
     { 
         name: "Pan", 
         list: {
-            macro: [GLUC_COMP]
+            macro: [GLUC_COMP, PROTEINA],
+            agua: AGUA_MEDIO,
         }, 
         info: "Riquísimo con aceite y pimentón." 
     },
@@ -26,6 +26,7 @@ const dataset: Food[] =  [
         name: "Pan integral", 
         list: {
             macro: [GLUC_COMP],
+            agua: AGUA_MEDIO,
             fibra: true
         }, 
         info: "Estupendo para bocadillos." 
@@ -33,8 +34,7 @@ const dataset: Food[] =  [
     { 
         name: "Lechuga", 
         list: {
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         },
         info: "En ensalada o en bocadillo." 
@@ -43,7 +43,8 @@ const dataset: Food[] =  [
         name: "Huevo", 
         list: {
             macro: [PROTEINA, LIPIDOS],
-            micro: [VITAMINAS]
+            agua: AGUA_ALTO,
+            minerals: [CA]
         }, 
         info: "Un alimento muy completo." 
     },
@@ -51,6 +52,8 @@ const dataset: Food[] =  [
         name: "Nueces", 
         list: {
             macro: [PROTEINA, LIPIDOS],
+            agua: AGUA_BAJO,
+            minerals: [CA],
             fibra: true
         }, 
         info: "Un puñado sacia mucho." 
@@ -59,8 +62,7 @@ const dataset: Food[] =  [
         name: "Fresas", 
         list: {
             macro: [GLUC_SIMP],
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         },
         info: "Contienen muy pocos azúcares, que son de liberación lenta." },
@@ -68,7 +70,7 @@ const dataset: Food[] =  [
         name: "Hummus de garbanzo", 
         list: {
             macro: [GLUC_COMP, PROTEINA],
-            agua: AGUA_MEDIO
+            agua: AGUA_ALTO
         }, 
         info: "Dipeando con zanahoria está buenísimo." 
     },
@@ -76,8 +78,7 @@ const dataset: Food[] =  [
         name: "Zumo de naranja", 
         list: {
             macro: [GLUC_SIMP],
-            micro: [VITAMINAS],
-            agua: AGUA_MEDIO
+            agua: AGUA_ALTO
         }, 
         info: "Una pieza entera de fruta es mejor." 
     },
@@ -85,8 +86,7 @@ const dataset: Food[] =  [
         name: "Manzana", 
         list: {
             macro: [GLUC_SIMP],
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         }, 
         info: "Deliciosamente crujiente. Los azúcares que contiene son de liberación lenta." 
@@ -95,6 +95,8 @@ const dataset: Food[] =  [
         name: "Patata asada", 
         list: {
             macro: [GLUC_COMP],
+            agua: AGUA_ALTO,
+            minerals: [K],
             fibra: true
         }, 
         info: "Un manjar, bien aliñada con aceite de pimentón." },
@@ -102,6 +104,7 @@ const dataset: Food[] =  [
         name: "Lentejas", 
         list: {
             macro: [GLUC_COMP, PROTEINA],
+            agua: AGUA_MEDIO,
             fibra: true
         }, 
         info: "Si las comes con arroz, obtienes todos los aminoácidos esenciales." },
@@ -109,6 +112,7 @@ const dataset: Food[] =  [
         name: "Avellanas", 
         list: {
             macro: [LIPIDOS, PROTEINA],
+            agua: AGUA_BAJO,
             fibra: true
         }, 
         info: "Crujientes y deliciosas." 
@@ -117,6 +121,7 @@ const dataset: Food[] =  [
         name: "Alubias", 
         list: {
             macro: [GLUC_COMP, PROTEINA],
+            agua: AGUA_ALTO,
             fibra: true
         }, 
         info: "Muy versátiles. Para platos de cuchara y ensaladas." 
@@ -124,8 +129,7 @@ const dataset: Food[] =  [
     { 
         name: "Judías verdes", 
         list: {
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         }, 
         info: "Imprescindibles en el hervido." 
@@ -134,7 +138,7 @@ const dataset: Food[] =  [
         name: "Aguacate", 
         list: {
             macro: [LIPIDOS],
-            micro: [VITAMINAS],
+            agua: AGUA_BAJO,
             fibra: true
         }, 
         info: "Muy rico en lípidos saludables." 
@@ -142,8 +146,7 @@ const dataset: Food[] =  [
     { 
         name: "Sandía", 
         list: {
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         }, 
         info: "Muy hidratante y refrescante. Los azúcares que contiene son de liberación lenta." 
@@ -152,7 +155,7 @@ const dataset: Food[] =  [
         name: "Aceitunas", 
         list: {
             macro: [LIPIDOS],
-            micro: [VITAMINAS],
+            agua: AGUA_MEDIO,
             fibra: true
         }, 
         info: "Los encurtidos son probióticoos." 
@@ -160,14 +163,16 @@ const dataset: Food[] =  [
     { 
         name: "Aceite de coco", 
         list: {
-            macro: [LIPIDOS]
+            macro: [LIPIDOS],
+            agua: AGUA_BAJO
         }, 
         info: "Desconocido para la mayoría de nosotros." 
     },
     { 
         name: "Patatas fritas", 
         list: {
-            macro: [GLUC_COMP, GLUC_SIMP, LIPIDOS]
+            macro: [GLUC_COMP, GLUC_SIMP, LIPIDOS],
+            agua: AGUA_MEDIO
         }, 
         info: "Mejor no abusar de ellas." 
     },
@@ -175,7 +180,7 @@ const dataset: Food[] =  [
         name: "Plátano", 
         list: {
             macro: [GLUC_COMP, GLUC_SIMP],
-            micro: [VITAMINAS, SALES],
+            agua: AGUA_MEDIO,
             fibra: true
         }, 
         info: "Muy rico en potasio. Los azúcares que contiene son de liberación lenta." 
@@ -183,8 +188,7 @@ const dataset: Food[] =  [
     { 
         name: "Pimiento rojo", 
         list: {
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         }, 
         info: "Contiene gran cantidad de vitamina C." 
@@ -193,6 +197,7 @@ const dataset: Food[] =  [
         name: "Guisantes", 
         list: {
             macro: [GLUC_COMP, PROTEINA],
+            agua: AGUA_MEDIO,
             fibra: true
         }, 
         info: "Fantástico encebollado, y con arroz." 
@@ -201,7 +206,7 @@ const dataset: Food[] =  [
         name: "Queso fresco", 
         list: {
             macro: [PROTEINA, LIPIDOS],
-            micro: [VITAMINAS, SALES]
+            agua: AGUA_MEDIO
         }, 
         info: "Óptimo para tomar en bocadillos y ensaladas." 
     },
@@ -209,22 +214,22 @@ const dataset: Food[] =  [
         name: "Queso curado", 
         list: {
             macro: [LIPIDOS, PROTEINA],
-            micro: [VITAMINAS, SALES]
+            agua: AGUA_BAJO
         }, 
         info: "Muy calórico." 
     },
     { 
         name: "Chocolate negro", 
         list: {
-            macro: [LIPIDOS, GLUC_SIMP]
+            macro: [LIPIDOS, GLUC_SIMP],
+            agua: AGUA_BAJO
         }, 
         info: "Cuanto más porcentaje de cacao, mejor." 
     },
     { 
         name: "Alcachofas", 
         list: {
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         }, 
         info: "La reina de la parrillada de verduras." 
@@ -232,15 +237,15 @@ const dataset: Food[] =  [
     { 
         name: "Chocolate blanco", 
         list: {
-            macro: [LIPIDOS, GLUC_SIMP]
+            macro: [LIPIDOS, GLUC_SIMP],
+            agua: AGUA_BAJO
         }, 
         info: "El chocolate blanco no existe." 
     },
     { 
         name: "Espinacas", 
         list: {
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         }, 
         info: "Contiene oxalatos y ácido fólico." 
@@ -248,14 +253,16 @@ const dataset: Food[] =  [
     { 
         name: "Aceite de oliva", 
         list: {
-            macro: [LIPIDOS]
+            macro: [LIPIDOS],
+            agua: AGUA_BAJO
         }, 
         info: "Sabroso, de nuestra tierra." 
     },
     { 
         name: "Aceite de girasol", 
         list: {
-            macro: [LIPIDOS]
+            macro: [LIPIDOS],
+            agua: AGUA_BAJO
         }, 
         info: "Aceite con sabor suave." 
     },
@@ -263,8 +270,7 @@ const dataset: Food[] =  [
         name: "Melón", 
         list: {
             macro: [GLUC_SIMP],
-            micro: [VITAMINAS, SALES],
-            agua: AGUA_MEDIO,
+            agua: AGUA_ALTO,
             fibra: true
         },
         info: "Hidratante y bajo en calorías. Los azúcares que contiene son de liberación lenta." 
@@ -272,21 +278,24 @@ const dataset: Food[] =  [
     { 
         name: "Bizcocho", 
         list: {
-            macro: [GLUC_SIMP, GLUC_COMP, LIPIDOS]
+            macro: [GLUC_SIMP, GLUC_COMP, LIPIDOS],
+            agua: AGUA_MEDIO
         }, 
         info: "Mejor si es casero, hecho con poco azúcar." 
     },
     { 
         name: "Galletas", 
         list: {
-            macro: [GLUC_SIMP, GLUC_COMP, LIPIDOS]
+            macro: [GLUC_SIMP, GLUC_COMP, LIPIDOS],
+            agua: AGUA_BAJO
         }, 
         info: "Conviene tomarlas solo esporádicamente." 
     },
     { 
         name: "Rosquilletas de pipas", 
         list: {
-            macro: [GLUC_SIMP, GLUC_COMP, LIPIDOS]
+            macro: [GLUC_SIMP, GLUC_COMP, LIPIDOS],
+            agua: AGUA_BAJO
         }, 
         info: "No conviene abusar de ellas." 
     }
